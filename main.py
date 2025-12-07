0007b796659a9bec7df121c3b537062791ec421b
aigerim2006 <aigerimhalbekova@gmail.com>
2025-12-08 00:02:51 +0600

    добавлено дз 4 задание 2

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import flet as ft
 # задание 2: добавить любимые имена
 # задание 4: добавить ограничение по длине истории
+
+# дз №4 задание 2 подсветка последнего имени
+
 def main(page: ft.Page):
     page.title = 'Мое первое приложение'
     page.theme_mode = ft.ThemeMode.DARK
     greeting_text = ft.Text(value='Hello world')
     greeting_history = []
     history_text = ft.Text(value='История приветствий')
-    favorite_names = []
-    favorites_text = ft.Text(value='Избранные имена:')
+    # favorite_names = []
+    # favorites_text = ft.Text(value='Избранные имена:')
+    
     def on_button_click(_):
         print(name_input.value)
         name = name_input.value.strip()
+
         if name:
-            greeting_text.value = f'Hello {name}'
+            # greeting_text.value = f'Hello {name}'
             greeting_text.color = None
+            greeting_text.value = None
+
+            greeting_text.spans = [ft.TextSpan('Hello '), ft.TextSpan(name, ft.TextStyle(color=ft.Colors.ORANGE))]
+
             name_input.value = ""
             greeting_history.append(name)
-            greeting_history[:] = greeting_history[-5:]
+            # greeting_history[:] = greeting_history[-5:]
             history_text.value = "История приветствий: \n" + "\n".join(greeting_history)
 
         else:
+            greeting_text.spans = []
+
             greeting_text.value = 'Введите корректное имя'
             greeting_text.color = ft.Colors.RED
             
@@ -30,14 +41,16 @@ def main(page: ft.Page):
         greeting_history.clear()
         history_text.value = "История приветствий:"
         page.update()
-    def add_to_favorites(_):
-        name = name_input.value.strip()
-        if name: 
-            favorite_names.append(name)
-            favorites_text.value = "Избранные имена:\n" + "\n".join(favorite_names)
-            page.update()
+
+    # def add_to_favorites(_):
+    #     name = name_input.value.strip()
+    #     if name: 
+    #         favorite_names.append(name)
+    #         favorites_text.value = "Избранные имена:\n" + "\n".join(favorite_names)
+    #         page.update()
 
     name_input = ft.TextField(label='Введите имя', on_submit=on_button_click, expand=True)
+
     send_button = ft.ElevatedButton(
         text = "Отправить",
         on_click = on_button_click,
@@ -47,7 +60,10 @@ def main(page: ft.Page):
     )
 
     clear_button = ft.IconButton(icon=ft.Icons.DELETE, on_click=clear_history)
-    favorite_button = ft.ElevatedButton(text="В избранное", on_click=add_to_favorites, color=ft.Colors.YELLOW)
-    page.add(greeting_text, ft.Row([name_input, send_button, clear_button]), history_text, ft.Row([favorite_button]), favorites_text)
+
+    # favorite_button = ft.ElevatedButton(text="В избранное", on_click=add_to_favorites, color=ft.Colors.YELLOW)
+    # page.add(greeting_text, ft.Row([name_input, send_button, clear_button]), history_text, ft.Row([favorite_button]), favorites_text)
+
+    page.add(greeting_text, ft.Row([name_input, send_button, clear_button]), history_text)
 
 ft.app(target=main)
